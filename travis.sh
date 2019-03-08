1e3108d054546b9b4f9bdff3d69d216ad5c7fdae
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-03-08 09:18:25 +0100

    WIP

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -x
 if [ "$TEST_SUITE" = "marketplaceeqp" ];
 then
     composer global config http-basic.repo.magento.com $MAGENTO_USERNAME $MAGENTO_PASSWORD
-    composer config repositories.repo-name composer repo.magento.com
+    composer config repositories.repo-name composer https://repo.magento.com
     composer require --prefer-source --no-interaction squizlabs/php_codesniffer magento/marketplace-eqp
     vendor/squizlabs/php_codesniffer/scripts/phpcs --config-set installed_paths vendor/magento/marketplace-eqp
     vendor/squizlabs/php_codesniffer/scripts/phpcs -n --standard="MEQP2" ./;
