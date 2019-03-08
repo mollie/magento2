038950f6107c5886ba4828370c5a7a0f5bccb937
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-03-08 08:51:53 +0100

    Extended on the Travis configuration: - Implemented the Magento Marketplace EQP code sniffer: https://github.com/magento/marketplace-eqp - Added the Magento tools: https://github.com/magento/marketplace-tools - Zip the contents and upload to Github when this build is a tag

---
+++ travis.sh
@@ -0,0 +1,23 @@
+set -e
+set -x
+
+if [ "$MEQP_CHECK" = "true" ];
+then
+    composer global config http-basic.repo.magento.com $MAGENTO_USERNAME $MAGENTO_PASSWORD
+    composer config repositories.repo-magento-com composer https://repo.magento.com
+    composer require --no-interaction magento/marketplace-eqp
+
+    vendor/bin/phpcs --config-set installed_paths vendor/magento/marketplace-eqp
+    vendor/bin/phpcs -p --ignore=*/vendor/*,*/Tests/* -n --severity=9 --standard="MEQP2" ./
+    vendor/bin/phpcs -p -n --severity=9 --standard="MEQP2" ./Tests/
+fi
+
+if [ "$TOOLS_CHECK" = "true" ];
+then
+    # Create the package
+    git archive --format=zip --output=Mollie_Payment.zip $TRAVIS_COMMIT
+
+    git clone https://github.com/magento/marketplace-tools.git
+
+    php marketplace-tools/validate_m2_package.php -d Mollie_Payment.zip
+fi
