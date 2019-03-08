3bfdc6f4a90e534a1715f56a24f26e295d2ccc96
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-03-08 15:03:39 +0100

    WIP

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 
-if [ "$TEST_SUITE" = "marketplaceeqp" ];
+if [ "$MEQP_CHECK" = "true" ];
 then
     composer global config http-basic.repo.magento.com $MAGENTO_USERNAME $MAGENTO_PASSWORD
     composer config repositories.repo-magento-com composer https://repo.magento.com
@@ -12,9 +12,12 @@ then
     vendor/bin/phpcs -p -n --severity=9 --standard="MEQP2" ./Tests/
 fi
 
-# Create the package
-git archive --format=zip --output=Mollie_Mpm.zip $TRAVIS_COMMIT
+if [ "$TOOLS_CHECK" = "true" ];
+then
+    # Create the package
+    git archive --format=zip --output=Mollie_Payment.zip $TRAVIS_COMMIT
 
-git clone https://github.com/magento/marketplace-tools.git
+    git clone https://github.com/magento/marketplace-tools.git
 
-php marketplace-tools/validate_m2_package.php -d Mollie_Mpm.zip
+    php marketplace-tools/validate_m2_package.php -d Mollie_Payment.zip
+fi
