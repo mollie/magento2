2d8c595c90b7fe7997f626e330ed156484e21a92
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-03-08 14:40:43 +0100

    WIP

--- travis.sh
+++ travis.sh
@@ -11,3 +11,10 @@ then
     vendor/bin/phpcs -p --ignore=*/vendor/*,*/Tests/* -n --severity=9 --standard="MEQP2" ./
     vendor/bin/phpcs -p -n --severity=9 --standard="MEQP2" ./Tests/
 fi
+
+# Create the package
+git archive --format=zip --output=Mollie_Mpm.zip $TRAVIS_COMMIT
+
+git clone https://github.com/magento/marketplace-tools.git
+
+php marketplace-tools/validate_m2_package.php Mollie_Mpm.zip
