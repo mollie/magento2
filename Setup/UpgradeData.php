da018f90005bec056ad45db1ee271c3f72e57b9f
Michiel Gerritsen <michiel@controlaltdelete.nl>
2019-05-17 19:56:28 +0200

    Change the default of the active configuration to show the payment methods correctly in the order grid

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -6,6 +6,8 @@
 
 namespace Mollie\Payment\Setup;
 
+use Magento\Config\Model\ResourceModel\Config;
+use Magento\Framework\App\ResourceConnection;
 use Magento\Sales\Setup\SalesSetupFactory;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
@@ -27,14 +29,30 @@ class UpgradeData implements UpgradeDataInterface
     private $salesSetupFactory;
 
     /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var Config
+     */
+    private $resourceConfig;
+
+    /**
      * UpgradeData constructor.
      *
      * @param SalesSetupFactory $salesSetupFactory
+     * @param ResourceConnection $resourceConnection
+     * @param Config $resourceConfig
      */
     public function __construct(
-        SalesSetupFactory $salesSetupFactory
+        SalesSetupFactory $salesSetupFactory,
+        ResourceConnection $resourceConnection,
+        Config $resourceConfig
     ) {
         $this->salesSetupFactory = $salesSetupFactory;
+        $this->resourceConnection = $resourceConnection;
+        $this->resourceConfig = $resourceConfig;
     }
 
     /**
@@ -49,6 +67,10 @@ class UpgradeData implements UpgradeDataInterface
 
         if (version_compare($context->getVersion(), "1.4.0", "<")) {
             $this->createMollieShipmentId($setup);
+        }
+
+        if (version_compare($context->getVersion(), '1.5.3', '<')) {
+            $this->upgradeActiveState();
         }
 
         $setup->endSetup();
@@ -67,5 +89,54 @@ class UpgradeData implements UpgradeDataInterface
          */
         $options = ['type' => 'varchar', 'visible' => false, 'required' => false];
         $salesSetup->addAttribute('shipment', 'mollie_shipment_id', $options);
+    }
+
+    /**
+     * See https://github.com/magento/magento2/issues/22231
+     * This is part of a fix that sets the default of the active config to 1 instead of 0. That's why we extract the
+     * current value from the database. If that is not 1 we set the value in the database to 0. This is to prevent
+     * that we turn on payment methods that are not active.
+     */
+    private function upgradeActiveState()
+    {
+        $paths = [
+            'payment/mollie_methods_bancontact/active',
+            'payment/mollie_methods_banktransfer/active',
+            'payment/mollie_methods_belfius/active',
+            'payment/mollie_methods_bitcoin/active',
+            'payment/mollie_methods_creditcard/active',
+            'payment/mollie_methods_ideal/active',
+            'payment/mollie_methods_kbc/active',
+            'payment/mollie_methods_paypal/active',
+            'payment/mollie_methods_paysafecard/active',
+            'payment/mollie_methods_sofort/active',
+            'payment/mollie_methods_inghomepay/active',
+            'payment/mollie_methods_giropay/active',
+            'payment/mollie_methods_eps/active',
+            'payment/mollie_methods_klarnapaylater/active',
+            'payment/mollie_methods_klarnasliceit/active',
+            'payment/mollie_methods_paymentlink/active',
+            'payment/mollie_methods_giftcard/active',
+            'payment/mollie_methods_przelewy24/active',
+        ];
+
+        foreach ($paths as $path) {
+            $this->setCorrectWebsiteDefault($path);
+        }
+    }
+
+    private function setCorrectWebsiteDefault($path)
+    {
+        $connection = $connection = $this->resourceConnection->getConnection();
+        $tableName = $this->resourceConnection->getTableName('core_config_data');
+
+        $query = 'select value from ' . $tableName . ' where scope = "default" and path = :path';
+        $result = $connection->fetchOne($query, ['path' => $path]);
+
+        if ($result !== false) {
+            return;
+        }
+
+        $this->resourceConfig->saveConfig($path, '0', 'default');
     }
 }
