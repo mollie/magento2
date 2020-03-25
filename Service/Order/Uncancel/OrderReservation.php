f3eacb330f3f37b2ac9ae755c18dc97b77100b89
Michiel Gerritsen <michiel@controlaltdelete.nl>
2020-03-25 10:24:14 +0100

    setup:di:compile fix for Magento 2.2 where MSI is not available

--- Service/Order/Uncancel/OrderReservation.php
+++ Service/Order/Uncancel/OrderReservation.php
@@ -3,6 +3,7 @@
 namespace Mollie\Payment\Service\Order\Uncancel;
 
 use Magento\Catalog\Model\Indexer\Product\Price\Processor;
+use Magento\Framework\App\ObjectManager;
 use Magento\InventorySalesApi\Api\Data\ItemToSellInterfaceFactory;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterfaceFactory;
@@ -20,63 +21,44 @@ class OrderReservation
     private $websiteRepository;
 
     /**
-     * @var SalesChannelInterfaceFactory
-     */
-    private $salesChannelFactory;
-
-    /**
-     * @var SalesEventInterfaceFactory
-     */
-    private $salesEventFactory;
-
-    /**
-     * @var PlaceReservationsForSalesEventInterface
-     */
-    private $placeReservationsForSalesEvent;
-
-    /**
      * @var Processor
      */
     private $priceIndexer;
+
     /**
-     * @var ItemToSellInterfaceFactory
+     * @var ObjectManager
      */
-    private $itemToSellFactory;
+    private $objectManager;
 
     public function __construct(
         WebsiteRepositoryInterface $websiteRepository,
         Processor $priceIndexer,
-        SalesChannelInterfaceFactory $salesChannelFactory,
-        SalesEventInterfaceFactory $salesEventFactory,
-        PlaceReservationsForSalesEventInterface $placeReservationsForSalesEvent,
-        ItemToSellInterfaceFactory $itemToSellFactory
+        ObjectManager $objectManager
     ) {
         $this->websiteRepository = $websiteRepository;
         $this->priceIndexer = $priceIndexer;
-        $this->salesChannelFactory = $salesChannelFactory;
-        $this->salesEventFactory = $salesEventFactory;
-        $this->placeReservationsForSalesEvent = $placeReservationsForSalesEvent;
-        $this->itemToSellFactory = $itemToSellFactory;
+        $this->objectManager = $objectManager;
     }
 
     public function execute(OrderItemInterface $orderItem)
     {
         $websiteId = $orderItem->getStore()->getWebsiteId();
         $websiteCode = $this->websiteRepository->getById($websiteId)->getCode();
-        $salesChannel = $this->salesChannelFactory->create([
+        $salesChannel = $this->objectManager->create(SalesChannelInterfaceFactory::class, [
             'data' => [
                 'type' => SalesChannelInterface::TYPE_WEBSITE,
                 'code' => $websiteCode
             ]
         ]);
 
-        $salesEvent = $this->salesEventFactory->create([
+        $salesEvent = $this->objectManager->create(SalesEventFactory::class, [
             'type' => 'order_uncanceled',
             'objectType' => SalesEventInterface::OBJECT_TYPE_ORDER,
             'objectId' => (string)$orderItem->getOrderId()
         ]);
 
-        $this->placeReservationsForSalesEvent->execute($this->getItemsToUncancel($orderItem), $salesChannel, $salesEvent);
+        $placeReservationsForSalesEvent = $this->objectManager->create(PlaceReservationsForSalesEvent::class);
+        $placeReservationsForSalesEvent->execute($this->getItemsToUncancel($orderItem), $salesChannel, $salesEvent);
 
         $this->priceIndexer->reindexRow($orderItem->getProductId());
     }
@@ -84,7 +66,7 @@ class OrderReservation
     private function getItemsToUncancel(OrderItemInterface $orderItem)
     {
         $itemsToUncancel = [];
-        $itemsToUncancel[] = $this->itemToSellFactory->create([
+        $itemsToUncancel[] = $this->objectManager->create(ItemToSellInterface::class, [
             'sku' => $orderItem->getSku(),
             // 0 - X = make it negative.
             'qty' => 0 - $orderItem->getQtyCanceled(),
@@ -92,7 +74,7 @@ class OrderReservation
 
         if ($orderItem->getHasChildren()) {
             foreach ($itemsToUncancel as $item) {
-                $itemsToUncancel[] = $this->itemToSellFactory->create([
+                $itemsToUncancel[] = $this->objectManager->create(ItemToSellInterface::class, [
                     'sku' => $orderItem->getSku(),
                     // 0 - X = make it negative.
                     'qty' => 0 - $orderItem->getQtyCanceled(),
