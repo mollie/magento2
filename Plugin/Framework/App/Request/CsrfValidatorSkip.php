ef709df8342fda91b5b85c939a6ccc634a4537ae
Michiel Gerritsen <michiel@controlaltdelete.nl>
2020-06-10 09:46:51 +0200

    Feature: Sometimes the Csrf validator skip did not work due to crappy modules messing with routers

--- Plugin/Framework/App/Request/CsrfValidatorSkip.php
+++ Plugin/Framework/App/Request/CsrfValidatorSkip.php
@@ -5,19 +5,31 @@ namespace Mollie\Payment\Plugin\Framework\App\Request;
 use Magento\Framework\App\ActionInterface;
 use Magento\Framework\App\Request\CsrfValidator;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\UrlInterface;
 
 class CsrfValidatorSkip
 {
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+
+    public function __construct(
+        UrlInterface $url
+    ) {
+        $this->url = $url;
+    }
+
     public function aroundValidate(
         CsrfValidator $subject,
         \Closure $proceed,
         RequestInterface $request,
         ActionInterface $action
     ) {
-        if ($request->getModuleName() == 'mollie') {
-            return;
+        if (strpos($this->url->getCurrentUrl(), 'mollie/checkout/webhook') !== false) {
+            return null;
         }
 
-        $proceed($request, $action);
+        return $proceed($request, $action);
     }
 }
