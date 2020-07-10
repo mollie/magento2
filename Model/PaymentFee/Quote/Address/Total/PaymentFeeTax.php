e059732b790155a7003316fe1f5a5b949444a887
Michiel Gerritsen <michiel@controlaltdelete.nl>
2020-07-10 09:56:27 +0200

    Bugfix: The payment fee tax is added to the tax overview in the checkout

--- Model/PaymentFee/Quote/Address/Total/PaymentFeeTax.php
+++ Model/PaymentFee/Quote/Address/Total/PaymentFeeTax.php
@@ -11,8 +11,12 @@ use Magento\Quote\Api\Data\ShippingAssignmentInterface;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address\Total;
 use Magento\Quote\Model\Quote\Address\Total\AbstractTotal;
+use Magento\Tax\Model\Sales\Total\Quote\CommonTaxCollector;
+use Mollie\Payment\Config;
+use Mollie\Payment\Exceptions\UnknownPaymentFeeType;
 use Mollie\Payment\Service\Config\PaymentFee as PaymentFeeConfig;
 use Mollie\Payment\Service\PaymentFee\Calculate;
+use Mollie\Payment\Service\PaymentFee\Result;
 
 class PaymentFeeTax extends AbstractTotal
 {
@@ -31,14 +35,21 @@ class PaymentFeeTax extends AbstractTotal
      */
     private $calculate;
 
+    /**
+     * @var Config
+     */
+    private $config;
+
     public function __construct(
         PaymentFeeConfig $paymentFeeConfig,
         PriceCurrencyInterface $priceCurrency,
-        Calculate $calculate
+        Calculate $calculate,
+        Config $config
     ) {
         $this->paymentFeeConfig = $paymentFeeConfig;
         $this->priceCurrency = $priceCurrency;
         $this->calculate = $calculate;
+        $this->config = $config;
     }
 
     /**
@@ -46,12 +57,12 @@ class PaymentFeeTax extends AbstractTotal
      * @param ShippingAssignmentInterface $shippingAssignment
      * @param Total $total
      * @return $this|AbstractTotal
+     * @throws UnknownPaymentFeeType
      */
     public function collect(Quote $quote, ShippingAssignmentInterface $shippingAssignment, Total $total)
     {
-        parent::collect($quote, $shippingAssignment, $total);
-
         if (!$shippingAssignment->getItems()) {
+            parent::collect($quote, $shippingAssignment, $total);
             return $this;
         }
 
@@ -60,6 +71,10 @@ class PaymentFeeTax extends AbstractTotal
 
         $total->addTotalAmount('tax', $amount);
         $total->addBaseTotalAmount('tax', $result->getTaxAmount());
+
+        $this->addAssociatedTaxable($shippingAssignment, $result, $quote);
+
+        parent::collect($quote, $shippingAssignment, $total);
 
         $extensionAttributes = $quote->getExtensionAttributes();
 
@@ -71,5 +86,34 @@ class PaymentFeeTax extends AbstractTotal
         $extensionAttributes->setBaseMolliePaymentFeeTax($result->getTaxAmount());
 
         return $this;
+    }
+
+    /**
+     * @param ShippingAssignmentInterface $shippingAssignment
+     * @param Result $result
+     * @param Quote $quote
+     */
+    private function addAssociatedTaxable(ShippingAssignmentInterface $shippingAssignment, Result $result, Quote $quote): void
+    {
+        $fullAmount = $this->priceCurrency->convert($result->getAmount());
+
+        $address = $shippingAssignment->getShipping()->getAddress();
+        $associatedTaxables = $address->getAssociatedTaxables();
+
+        $method = $quote->getPayment()->getMethod();
+        $taxClass = $this->config->paymentSurchargeTaxClass($method, $quote->getStoreId());
+
+        $associatedTaxables[] = [
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_TYPE => 'mollie_payment_fee_tax',
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_CODE => 'mollie_payment_fee_tax',
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_UNIT_PRICE => $fullAmount,
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_BASE_UNIT_PRICE => $result->getAmount(),
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_QUANTITY => 1,
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_TAX_CLASS_ID => $taxClass,
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_PRICE_INCLUDES_TAX => false,
+            CommonTaxCollector::KEY_ASSOCIATED_TAXABLE_ASSOCIATION_ITEM_CODE => null,
+        ];
+
+        $address->setAssociatedTaxables($associatedTaxables);
     }
 }
