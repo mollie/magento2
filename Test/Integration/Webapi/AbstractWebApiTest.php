6ca0e5e880862a1525f47618e5f97b9a160d97c2
Michiel Gerritsen <michiel@controlaltdelete.nl>
2020-08-14 10:39:20 +0200

    Bugfix: Add a valid rest api schema

---
+++ Test/Integration/Webapi/AbstractWebApiTest.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright Magmodules.eu. All rights reserved.
+ *  * See COPYING.txt for license details.
+ */
+
+namespace Mollie\Payment\Test\Integration\Webapi;
+
+use Magento\Webapi\Model\Config\ClassReflector;
+use Mollie\Payment\Test\Integration\IntegrationTestCase;
+
+abstract class AbstractWebApiTest extends IntegrationTestCase
+{
+    /**
+     * @var string
+     */
+    protected $class;
+
+    /**
+     * @var string[]
+     */
+    protected $methods;
+
+    public function testValidatesForSwagger()
+    {
+        if (!$this->class || !$this->methods) {
+            throw new \Exception('Please set the $class and $method variables');
+        }
+
+        /** @var ClassReflector $reflector */
+        $reflector = $this->objectManager->get(ClassReflector::class);
+
+        $implementations = array_merge([$this->class], class_implements($this->class));
+        foreach ($implementations as $implementation) {
+            $reflector->reflectClassMethods($implementation, $this->methods);
+            $this->addToAssertionCount(1);
+        }
+    }
+}
