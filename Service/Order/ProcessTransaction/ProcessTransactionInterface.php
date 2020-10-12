88d594a83f41534722565dd0edcaa3d1bf4147a0
Michiel Gerritsen <michiel@controlaltdelete.nl>
2020-10-12 14:54:15 +0200

    Bugfix: handle coupon refunds correct

---
+++ Service/Order/ProcessTransaction/ProcessTransactionInterface.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright Magmodules.eu. All rights reserved.
+ *  See COPYING.txt for license details.
+ */
+
+namespace Mollie\Payment\Service\Order\ProcessTransaction;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Mollie\Api\Resources\Order as MollieOrder;
+use Mollie\Api\Resources\Payment;
+
+interface ProcessTransactionInterface
+{
+    /**
+     * This function gets called on a response from Mollie. Only $mollieOrder or $molliePayment will be set, not both.
+     *
+     * @param OrderInterface $order
+     * @param MollieOrder|null $mollieOrder
+     * @param Payment|null $molliePayment
+     * @return void
+     */
+    public function process(OrderInterface $order, MollieOrder $mollieOrder = null, Payment $molliePayment = null);
+}
