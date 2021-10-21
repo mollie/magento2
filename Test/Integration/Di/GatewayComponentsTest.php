50a90e55d3254bb3aae31152be2129260a2a6968
Michiel Gerritsen <michiel@controlaltdelete.nl>
2021-10-21 14:24:13 +0200

    Bugfix: Limit methods by country settings

---
+++ Test/Integration/Di/GatewayComponentsTest.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * Copyright Magmodules.eu. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Mollie\Payment\Test\Integration\Di;
+
+use Magento\Framework\ObjectManager\ConfigInterface;
+use Magento\TestFramework\ObjectManager\Config;
+use Mollie\Payment\Test\Integration\IntegrationTestCase;
+
+class GatewayComponentsTest extends IntegrationTestCase
+{
+    public function testHasAValidatorPool()
+    {
+        /** @var Config $config */
+        $config = $this->objectManager->get(ConfigInterface::class);
+
+        foreach ($this->getMethods() as $method) {
+            $name = $method['name'];
+
+            $this->assertArrayHasKey('Mollie' . $name . 'ValidatorPool', $config->getVirtualTypes());
+        }
+    }
+
+    public function testHasTheValidatorPoolConfigured()
+    {
+        $arguments = $this->getObjectManagerArguments();
+
+        foreach ($this->getMethods() as $method) {
+            $class = $method['class'];
+            $name  = $method['name'];
+            $classArguments = $arguments[$class];
+
+            $this->assertArrayHasKey('validatorPool', $classArguments, $name . ' does not have a ValidatorPool');
+            $this->assertEquals('Mollie' . $name . 'ValidatorPool', $classArguments['validatorPool']['instance']);
+        }
+    }
+
+    public function testHasACountryValidator()
+    {
+        /** @var Config $config */
+        $config = $this->objectManager->get(ConfigInterface::class);
+
+        foreach ($this->getMethods() as $method) {
+            $name = $method['name'];
+
+            $this->assertArrayHasKey('Mollie' . $name . 'CountryValidator', $config->getVirtualTypes());
+        }
+    }
+
+    private function getObjectManagerArguments(): array
+    {
+        static $arguments = null;
+
+        if ($arguments) {
+            return $arguments;
+        }
+
+        /** @var Config $config */
+        $config = $this->objectManager->get(ConfigInterface::class);
+
+        $reflectionObject = new \ReflectionObject($config);
+        $reflectionProperty = $reflectionObject->getProperty('_arguments');
+        $reflectionProperty->setAccessible(true);
+
+        $arguments = $reflectionProperty->getValue($config);
+
+        return $arguments;
+    }
+
+    private function getMethods(): array
+    {
+        $keys = array_keys($this->getObjectManagerArguments());
+
+        $methods = array_filter($keys, function ($key) {
+            return strpos($key, 'Mollie\\Payment\\Model\\Methods') !== false;
+        });
+
+        return array_map(function ($key) {
+            $parts = explode('\\', $key);
+
+            return [
+                'class' => $key,
+                'name' => end($parts),
+            ];
+        }, $methods);
+    }
+}
