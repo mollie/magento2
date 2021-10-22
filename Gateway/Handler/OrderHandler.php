3ea1f3e43513c925e82b0b4a204c75dd43061341
Michiel Gerritsen <michiel@controlaltdelete.nl>
2021-10-22 19:23:59 +0200

    Bugfix: Make sure the correct state when placing the order

--- Gateway/Handler/OrderHandler.php
+++ Gateway/Handler/OrderHandler.php
@@ -8,6 +8,8 @@ namespace Mollie\Payment\Gateway\Handler;
 
 use Magento\Payment\Gateway\Data\PaymentDataObject;
 use Magento\Payment\Gateway\Response\HandlerInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
 
 class OrderHandler implements HandlerInterface
 {
@@ -16,8 +18,14 @@ class OrderHandler implements HandlerInterface
         /** @var PaymentDataObject $paymentDataObject */
         $paymentDataObject = $handlingSubject['payment'];
         $payment = $paymentDataObject->getPayment();
+
+        // Make sure the transaction is marked as pending so we don't get the wrong order state.
+        $payment->setIsTransactionPending(true);
+
+        /** @var OrderInterface $order */
         $order = $payment->getOrder();
 
+        // Don't send the email just yet.
         $order->setCanSendNewEmailFlag(false);
     }
 }
