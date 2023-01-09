8fd420f3a8296c420e3db25522c93b37d7bedb28
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-01-09 10:01:23 +0100

    Bugfix: Let Magento Vault adhere to the active configuration field

---
+++ Model/Data/CreditcardVaultConfig.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Mollie\Payment\Model\Data;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Payment\Gateway\ConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Mollie\Payment\Config;
+
+class CreditcardVaultConfig implements ConfigInterface
+{
+    const DEFAULT_PATH_PATTERN = 'payment/%s/%s';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var string|null
+     */
+    private $methodCode;
+
+    /**
+     * @var string|null
+     */
+    private $pathPattern;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param string|null $methodCode
+     * @param string $pathPattern
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        string $methodCode = null,
+        string $pathPattern = self::DEFAULT_PATH_PATTERN
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->methodCode = $methodCode;
+        $this->pathPattern = $pathPattern;
+    }
+
+    /**
+     * Sets method code
+     *
+     * @param string $methodCode
+     * @return void
+     */
+    public function setMethodCode($methodCode)
+    {
+        $this->methodCode = $methodCode;
+    }
+
+    /**
+     * Sets path pattern
+     *
+     * @param string $pathPattern
+     * @return void
+     */
+    public function setPathPattern($pathPattern)
+    {
+        $this->pathPattern = $pathPattern;
+    }
+
+    /**
+     * Retrieve information from payment configuration
+     *
+     * @param string $field
+     * @param int|null $storeId
+     *
+     * @return mixed
+     */
+    public function getValue($field, $storeId = null)
+    {
+        if ($this->methodCode === null || $this->pathPattern === null) {
+            return null;
+        }
+
+        $path = sprintf($this->pathPattern, $this->methodCode, $field);
+
+        if ($field == 'active') {
+            $path = Config::GENERAL_ENABLE_MAGENTO_VAULT;
+        }
+
+        return $this->scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
