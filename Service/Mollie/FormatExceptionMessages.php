b9553476c043f054778ad62bd4af5e336640fead
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-07-17 12:40:07 +0200

    Improvement: Allow to translate error messages #640

--- Service/Mollie/FormatExceptionMessages.php
+++ Service/Mollie/FormatExceptionMessages.php
@@ -15,11 +15,21 @@ class FormatExceptionMessages
         'The webhook URL is invalid because it is unreachable from Mollie\'s point of view' => 'The webhook URL is invalid because it is unreachable from Mollie\'s point of view. View this article for more information: https://github.com/mollie/magento2/wiki/Webhook-Communication-between-your-Magento-webshop-and-Mollie',
     ];
 
+    public function __construct(
+        array $allowedErrorMessages = []
+    ) {
+        $this->allowedErrorMessages = array_merge($this->allowedErrorMessages, $allowedErrorMessages);
+    }
+
     public function execute(\Exception $exception, MethodInterface $methodInstance = null): string
     {
+        // Make sure this can be picked up by bin/magento i18n:collect-phrases
+        // __('The billing country is not supported for this payment method.')
+        // __('A billing organization name is required for this payment method.')
+
         foreach ($this->allowedErrorMessages as $message) {
             if (stripos($exception->getMessage(), $message) !== false) {
-                return $message;
+                return __($message);
             }
         }
 
