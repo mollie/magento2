00eca2828bf675e8b08bf951db86db630403adce
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-07-20 13:35:31 +0200

    End-2-end testing improvements

--- Test/End-2-end/cypress/support/pages/backend/ShipmentPage.js
+++ Test/End-2-end/cypress/support/pages/backend/ShipmentPage.js
@@ -7,6 +7,11 @@ export default class ShipmentPage {
 
     ship() {
         cy.get('#system_messages').should('have.length.gte', 0);
+
+        // Last element on the page so javascript has time to load
+        cy.get('.magento-version').should('be.visible');
+        cy.wait(500);
+
         cy.get('[data-ui-id="order-items-submit-button"]').should('be.enabled').click();
 
         cy.get('[data-ui-id="sales-order-tabs-tab-sales-order-view-tabs"] .ui-state-active').should('be.visible');
