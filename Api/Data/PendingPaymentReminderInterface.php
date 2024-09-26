d2741117edc48368621adf4ecc01e91c7a1b6868
Michiel Gerritsen <michiel@controlaltdelete.nl>
2024-09-26 13:42:13 +0200

    Improvement: Do not depend on the sales_order table for pending payment reminders

--- Api/Data/PendingPaymentReminderInterface.php
+++ Api/Data/PendingPaymentReminderInterface.php
@@ -11,6 +11,8 @@ use Magento\Framework\Api\ExtensibleDataInterface;
 interface PendingPaymentReminderInterface extends ExtensibleDataInterface
 {
     const ENTITY_ID = 'entity_id';
+    const CUSTOMER_ID = 'customer_id';
+    const HASH = 'hash';
     const ORDER_ID = 'order_id';
 
     /**
@@ -23,6 +25,28 @@ interface PendingPaymentReminderInterface extends ExtensibleDataInterface
      * @return int
      */
     public function getEntityId();
+
+    /**
+     * @param int|null $customerId
+     * @return \Mollie\Payment\Api\Data\PendingPaymentReminderInterface
+     */
+    public function setCustomerId(int $customerId = null);
+
+    /**
+     * @return int
+     */
+    public function getCustomerId();
+
+    /**
+     * @param string|null $hash
+     * @return \Mollie\Payment\Api\Data\PendingPaymentReminderInterface
+     */
+    public function setHash(string $hash = null);
+
+    /**
+     * @return string
+     */
+    public function getHash();
 
     /**
      * @param int $orderId
