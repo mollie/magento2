c5744e838f94d9d1c9ed999570dfac52b35d14fa
Michiel Gerritsen <michiel@controlaltdelete.nl>
2026-07-06 11:50:19 +0200

    Fix failing codesniffs

--- Test/Integration/etc/adminhtml/methods/CaptureDelayConfigurationTest.php
+++ Test/Integration/etc/adminhtml/methods/CaptureDelayConfigurationTest.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 namespace Mollie\Payment\Test\Integration\etc\adminhtml\methods;
 
 class CaptureDelayConfigurationTest extends AbstractXmlConfiguration
